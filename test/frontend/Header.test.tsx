a18ebd0f963ef60875631d083df333e5c85832d4
Korniyko <cool.kornijka36@gmail.com>
2024-01-11 12:35:01 +0200

    newLoad

--- test/frontend/Header.test.tsx
+++ test/frontend/Header.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { render, screen } from '@testing-library/react';
+import { Router, MemoryRouter, Route } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Header from "../../src/frontend/components/Header";
+import { createMemoryHistory } from "history";
 
 describe('header test', () => {
     it('correct Header Render', () => {
@@ -9,5 +11,21 @@ describe('header test', () => {
         )
         expect(screen.queryByTestId('header')).toBeTruthy();
         expect(screen.getAllByText('Login').length).toBe(1);
+    });
+    it('handle login click', () => {
+        const history = createMemoryHistory()
+        render(
+
+            <Router history={history}>
+                <Route path={'/'} component={Header} />
+            </Router>
+
+        )
+        const button = screen.getAllByText('Login')
+        fireEvent.click(button[0])
+        expect(history.location.pathname).toBe('/login')
+
+
     })
 })
+
